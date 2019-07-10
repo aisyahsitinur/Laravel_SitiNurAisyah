cab1f891a833d1f0dace8972913a69f40479dace
aisyahsitinur <aisyahsitinur438@gamil.com>
2019-07-09 23:47:39 -0600

    episode 2

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::prefix('admin')->group(Function(){
+	Route::get('/',function(){
+		return view('admin.main');
+	})->name('admin.home');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
